8458dbd6d9282da84ffb989a24a60c5c8a726761
SyberiaK <syberiakey@gmail.com>
2022-08-19 16:28:08 +0500

    Little conftest polish

--- conftest.py
+++ conftest.py
@@ -2,9 +2,9 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope='function')
 def browser():
-    print('\nstart browser for test..')
+    print('\nstart browser for test...')
     browser = webdriver.Chrome()
     yield browser
     print('\nquit browser...')
